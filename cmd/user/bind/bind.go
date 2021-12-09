f8244d72e28088372b6db211afa47b31fbbaf36b
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-09 11:51:26 +0100

    feat(user): bind command

--- cmd/user/bind/bind.go
+++ cmd/user/bind/bind.go
@@ -1,9 +1,11 @@
 package bind
 
 import (
-	"fmt"
 	"taikun-cli/api"
+	"taikun-cli/cmd/cmdutils"
 
+	"github.com/itera-io/taikungoclient/client/user_projects"
+	"github.com/itera-io/taikungoclient/models"
 	"github.com/spf13/cobra"
 )
 
@@ -19,7 +21,7 @@ func NewCmdBind(apiClient *api.Client) *cobra.Command {
 		Use:   "bind",
 		Short: "Bind a user to a project",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return bindRun(&opts)
+			return bindRun(&opts, apiClient)
 		},
 	}
 
@@ -32,8 +34,18 @@ func NewCmdBind(apiClient *api.Client) *cobra.Command {
 	return cmd
 }
 
-func bindRun(opts *BindOptions) (err error) {
-	fmt.Printf("Binding user %s to project with ID %d\n", opts.Username, opts.ProjectID)
-	// FIXME
+func bindRun(opts *BindOptions, apiClient *api.Client) (err error) {
+	body := &models.BindProjectsCommand{
+		UserName: opts.Username,
+		Projects: []*models.UpdateUserProjectDto{
+			{
+				ProjectID: int32(opts.ProjectID),
+				IsBound:   true,
+			},
+		},
+	}
+
+	params := user_projects.NewUserProjectsBindProjectsParams().WithV(cmdutils.ApiVersion).WithBody(body)
+	_, err = apiClient.Client.UserProjects.UserProjectsBindProjects(params, apiClient)
 	return
 }
