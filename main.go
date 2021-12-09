6089f5a9dde2d753c3e3b3f5480bf5c963b4a253
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-09 17:07:13 +0100

    fix: set exit code on error

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"os"
 	"taikun-cli/cmd/root"
 )
 
 func main() {
 	rootCmd := root.NewCmdRoot()
-	rootCmd.Execute()
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
