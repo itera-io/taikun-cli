045e1ab6e2e3cc05092f9836483e744ee13f8fb0
Antoine <antoine.labarussias@epita.fr>
2021-12-13 13:58:44 +0100

    feat(billing-credential): add delete

---
+++ cmd/billingcredential/delete/delete.go
@@ -0,0 +1,43 @@
+package delete
+
+import (
+	"fmt"
+
+	"taikun-cli/api"
+	"taikun-cli/cmd/cmdutils"
+
+	"github.com/itera-io/taikungoclient/client/ops_credentials"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdDelete() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "delete <billing-credential-id>",
+		Short: "Delete a billing credential",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			id, err := cmdutils.Atoi32(args[0])
+			if err != nil {
+				return fmt.Errorf("the given id must be a number")
+			}
+			return deleteRun(id)
+		},
+	}
+
+	return cmd
+}
+
+func deleteRun(id int32) (err error) {
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	params := ops_credentials.NewOpsCredentialsDeleteParams().WithV(cmdutils.ApiVersion).WithID(id)
+	_, _, err = apiClient.Client.OpsCredentials.OpsCredentialsDelete(params, apiClient)
+	if err == nil {
+		fmt.Println("Billing Credential deleted")
+	}
+
+	return
+}
