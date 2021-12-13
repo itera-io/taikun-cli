c05d7fae3f2db46b6d22ada2f24d4d9de4829292
Antoine <antoine.labarussias@epita.fr>
2021-12-13 13:59:02 +0100

    feat(billing-credential): register commands

---
+++ cmd/billingcredential/billing_credentials.go
@@ -0,0 +1,23 @@
+package billingcredential
+
+import (
+	"taikun-cli/cmd/billingcredential/create"
+	"taikun-cli/cmd/billingcredential/delete"
+	"taikun-cli/cmd/billingcredential/list"
+
+	"github.com/spf13/cobra"
+)
+
+func NewCmdBillingCredential() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "billing-credential <command>",
+		Short:   "Manage Billing Credentials",
+		Aliases: []string{"operation-credentials"},
+	}
+
+	cmd.AddCommand(create.NewCmdCreate())
+	cmd.AddCommand(list.NewCmdList())
+	cmd.AddCommand(delete.NewCmdDelete())
+
+	return cmd
+}
