40058a7f5c255381ea83e0abec0aca4d8b8f4f61
Antoine <antoine.labarussias@epita.fr>
2021-12-13 11:36:59 +0100

    refactor(access-profiles): do not use flag in delete

--- cmd/accessprofile/delete/delete.go
+++ cmd/accessprofile/delete/delete.go
@@ -10,34 +10,37 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type DeleteOptions struct {
-	ID int32
-}
-
 func NewCmdDelete() *cobra.Command {
-	var opts DeleteOptions
-
 	cmd := &cobra.Command{
-		Use:   "delete",
+		Use:   "delete <access-profile-id>",
 		Short: "Delete an access profile",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return fmt.Errorf("accepts 1 arg, received %d", len(args))
+			}
+			_, err := cmdutils.Atoi32(args[0])
+			if err != nil {
+				return fmt.Errorf("the given id must be a number")
+			}
+
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return deleteRun(&opts)
+			id, _ := cmdutils.Atoi32(args[0])
+			return deleteRun(id)
 		},
 	}
-
-	cmd.Flags().Int32Var(&opts.ID, "id", 0, "ID (required)")
-	cmdutils.MarkFlagRequired(cmd, "id")
 
 	return cmd
 }
 
-func deleteRun(opts *DeleteOptions) (err error) {
+func deleteRun(id int32) (err error) {
 	apiClient, err := api.NewClient()
 	if err != nil {
 		return
 	}
 
-	params := access_profiles.NewAccessProfilesDeleteParams().WithV(cmdutils.ApiVersion).WithID(opts.ID)
+	params := access_profiles.NewAccessProfilesDeleteParams().WithV(cmdutils.ApiVersion).WithID(id)
 	_, _, err = apiClient.Client.AccessProfiles.AccessProfilesDelete(params, apiClient)
 	if err == nil {
 		fmt.Println("Access Profile deleted")
