60447324be3f4b2799a84d100e8c49bde9313e77
Antoine <antoine.labarussias@epita.fr>
2021-12-13 16:27:38 +0100

    feat(kubernetes-profiles): create

---
+++ cmd/kubernetesprofile/create/create.go
@@ -0,0 +1,65 @@
+package create
+
+import (
+	"taikun-cli/api"
+	"taikun-cli/cmd/cmdutils"
+
+	"github.com/itera-io/taikungoclient/client/kubernetes_profiles"
+	"github.com/itera-io/taikungoclient/models"
+	"github.com/spf13/cobra"
+)
+
+type CreateOptions struct {
+	AllowSchedulingOnMaster bool
+	ExposeNodePortOnBastion bool
+	Name                    string
+	OctaviaEnabled          bool
+	OrganizationID          int32
+	TaikunLBEnabled         bool
+}
+
+func NewCmdCreate() *cobra.Command {
+	var opts CreateOptions
+
+	cmd := &cobra.Command{
+		Use:   "create <name>",
+		Short: "Create a kubernetes profile",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			opts.Name = args[0]
+			return createRun(&opts)
+		},
+	}
+
+	cmd.Flags().Int32VarP(&opts.OrganizationID, "organization-id", "o", 0, "Organization ID")
+	cmd.Flags().BoolVar(&opts.AllowSchedulingOnMaster, "allow-master-scheduling", false, "Allow scheduling on master nodes")
+	cmd.Flags().BoolVar(&opts.ExposeNodePortOnBastion, "expose-node-port-on-bastion", false, "Expose Node Port on Bastion")
+	cmd.Flags().BoolVar(&opts.OctaviaEnabled, "octavia-enabled", false, "Enable Octavia Load Balancer")
+	cmd.Flags().BoolVar(&opts.TaikunLBEnabled, "taikun-lb-enabled", false, "Enable Taikun Load Balancer")
+
+	return cmd
+}
+
+func createRun(opts *CreateOptions) (err error) {
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	body := &models.CreateKubernetesProfileCommand{
+		AllowSchedulingOnMaster: opts.AllowSchedulingOnMaster,
+		ExposeNodePortOnBastion: opts.ExposeNodePortOnBastion,
+		Name:                    opts.Name,
+		OctaviaEnabled:          opts.OctaviaEnabled,
+		OrganizationID:          opts.OrganizationID,
+		TaikunLBEnabled:         opts.TaikunLBEnabled,
+	}
+
+	params := kubernetes_profiles.NewKubernetesProfilesCreateParams().WithV(cmdutils.ApiVersion).WithBody(body)
+	response, err := apiClient.Client.KubernetesProfiles.KubernetesProfilesCreate(params, apiClient)
+	if err == nil {
+		cmdutils.PrettyPrint(response.Payload)
+	}
+
+	return
+}
