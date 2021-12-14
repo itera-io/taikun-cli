2b1a4575e28d3e4cdc678c6f53f5647d248c3085
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-14 16:56:46 +0100

    fix(flavor): check limit flag is positive

--- cmd/flavor/list/list.go
+++ cmd/flavor/list/list.go
@@ -29,6 +29,9 @@ func NewCmdList() *cobra.Command {
 			if err != nil {
 				return fmt.Errorf("the given ID must be a number")
 			}
+			if opts.Limit < 0 {
+				return fmt.Errorf("limit flag must be positive")
+			}
 			opts.ProjectID = projectID
 			return listRun(&opts)
 		},
