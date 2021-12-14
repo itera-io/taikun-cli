7ed1bf3b9884b96d7f3ced740d70fb21bacf0274
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-14 16:55:00 +0100

    fix(backup-credential): check limit flag is positive

--- cmd/backupcredential/list/list.go
+++ cmd/backupcredential/list/list.go
@@ -1,6 +1,7 @@
 package list
 
 import (
+	"fmt"
 	"taikun-cli/api"
 	"taikun-cli/cmd/cmdutils"
 
@@ -22,6 +23,9 @@ func NewCmdList() *cobra.Command {
 		Short: "List backup credentials",
 		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if opts.Limit < 0 {
+				return fmt.Errorf("limit flag must be positive")
+			}
 			return listRun(&opts)
 		},
 	}
