781896c40e45c2153ccedfcd0f9db38d9336c815
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-15 12:58:11 +0100

    feat(utils): add table pretty printer

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-openapi/runtime v0.21.0
 	github.com/go-openapi/strfmt v0.21.1
 	github.com/itera-io/taikungoclient v0.0.0-20211216105107-2c564cb95039
+	github.com/jedib0t/go-pretty/v6 v6.2.4
 	github.com/spf13/cobra v1.2.1
 )
 
@@ -25,12 +26,14 @@ require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.mongodb.org/mongo-driver v1.7.5 // indirect
 	golang.org/x/net v0.0.0-20210421230115-4e50805a0758 // indirect
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- utils/format.go
+++ utils/format.go
@@ -3,6 +3,12 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/jedib0t/go-pretty/v6/text"
 )
 
 var emptyStruct struct{}
@@ -10,14 +16,98 @@ var emptyStruct struct{}
 const prettyPrintPrefix = ""
 const prettyPrintIndent = "    "
 
-func PrettyPrintJson(data interface{}) error {
+func marshalJsonData(data interface{}) []byte {
 	if data == nil {
 		data = emptyStruct
 	}
 	jsonBytes, err := json.MarshalIndent(data, prettyPrintPrefix, prettyPrintIndent)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	fmt.Println(string(jsonBytes))
-	return nil
+	return jsonBytes
+}
+
+func PrettyPrintJson(data interface{}) {
+	fmt.Println(string(marshalJsonData(data)))
+}
+
+func structToMap(data interface{}) map[string]interface{} {
+	var m map[string]interface{}
+	if err := json.Unmarshal(marshalJsonData(data), &m); err != nil {
+		log.Fatal(err)
+	}
+	return m
+}
+
+func structsToMaps(structs []interface{}) []map[string]interface{} {
+	maps := make([]map[string]interface{}, len(structs))
+	for i, s := range structs {
+		maps[i] = structToMap(s)
+	}
+	return maps
+}
+
+func isUpperCase(c byte) bool {
+	return c >= 'A' && c <= 'Z'
+}
+
+func isLowerCase(c byte) bool {
+	return c >= 'a' && c <= 'z'
+}
+
+func toUpper(c byte) byte {
+	if isLowerCase(c) {
+		c -= 'a'
+		c += 'A'
+	}
+	return c
+}
+
+func formatFieldName(fieldName string) string {
+	var stringBuilder strings.Builder
+	previous := fieldName[0]
+	stringBuilder.WriteByte(toUpper(fieldName[0]))
+	for i := 1; i < len(fieldName); i++ {
+		if isLowerCase(previous) && isUpperCase(fieldName[i]) {
+			stringBuilder.WriteByte(' ')
+		}
+		stringBuilder.WriteByte(fieldName[i])
+		previous = fieldName[i]
+	}
+	return stringBuilder.String()
+}
+
+func fieldsToHeaderRow(fields []string) table.Row {
+	row := make([]interface{}, len(fields))
+	for i, field := range fields {
+		row[i] = formatFieldName(field)
+	}
+	return row
+}
+
+func newTable() table.Writer {
+	t := table.NewWriter()
+	t.SetOutputMirror(os.Stdout)
+	t.SetStyle(table.StyleDefault)
+	t.Style().Options = table.OptionsNoBorders
+	t.Style().Format.Header = text.FormatDefault
+	return t
+}
+
+func PrettyPrintTable(resources interface{}, fields ...string) {
+	t := newTable()
+
+	t.AppendHeader(fieldsToHeaderRow(fields))
+	t.AppendSeparator()
+
+	resourceMaps := structsToMaps(resources.([]interface{}))
+	for _, resourceMap := range resourceMaps {
+		row := make([]interface{}, len(fields))
+		for i, field := range fields {
+			row[i] = resourceMap[field]
+		}
+		t.AppendRow(table.Row(row))
+	}
+
+	t.Render()
 }
