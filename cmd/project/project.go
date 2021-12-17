5c7f8bacd65fcebee3d61d404b1c46480382b4dc
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-17 17:06:32 +0100

    feat(project): list command

--- cmd/project/project.go
+++ cmd/project/project.go
@@ -1,12 +1,18 @@
 package project
 
-import "github.com/spf13/cobra"
+import (
+	"taikun-cli/cmd/project/list"
+
+	"github.com/spf13/cobra"
+)
 
 func NewCmdProject() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "project <command>",
 		Short: "Manage projects",
 	}
+
+	cmd.AddCommand(list.NewCmdList())
 
 	return cmd
 }
