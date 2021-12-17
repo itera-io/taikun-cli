0ee3b0ff1e85727fd15aceccbc97c630ac3c8594
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-17 15:33:02 +0100

    chore(utils): add helpers for validation output

--- cmd/cmderr/errors.go
+++ cmd/cmderr/errors.go
@@ -1,7 +1,14 @@
 package cmderr
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 var NegativeLimitFlagError = errors.New("The --limit flag must be positive.")
 var OutputFormatInvalidError = errors.New("Unknown output format.")
 var WrongIDArgumentFormatError = errors.New("The ID argument must be a number.")
+
+func CheckFailureError(name string) error {
+	return fmt.Errorf("%s is not valid.", name)
+}
