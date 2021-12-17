02b6880480998d80110f506282d27684a97292d8
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-17 11:13:25 +0100

    feat(utils): helper to get sort-by options from struct tags

--- cmd/cmdutils/cmdutils.go
+++ cmd/cmdutils/cmdutils.go
@@ -2,6 +2,7 @@ package cmdutils
 
 import (
 	"log"
+	"reflect"
 
 	"github.com/spf13/cobra"
 )
@@ -22,4 +23,18 @@ func RegisterStaticFlagCompletion(cmd *cobra.Command, flagName string, values ..
 	RegisterFlagCompletionFunc(cmd, flagName, func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		return values, cobra.ShellCompDirectiveDefault
 	})
+}
+
+func getStructFieldJsonTag(structType reflect.Type, i int) string {
+	return structType.Field(i).Tag.Get("json")
+}
+
+func GetSortByOptions(s interface{}) []string {
+	structType := reflect.ValueOf(s).Type()
+	structFieldCount := structType.NumField()
+	structFieldJsonTags := make([]string, structFieldCount)
+	for i := 0; i < structFieldCount; i++ {
+		structFieldJsonTags[i] = getStructFieldJsonTag(structType, i)
+	}
+	return structFieldJsonTags
 }
