0e4a51401c5633d02e635778d10f736328d4d223
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-20 13:51:55 +0100

    feat(project): create w/ expiration date

--- cmd/cmderr/errors.go
+++ cmd/cmderr/errors.go
@@ -2,6 +2,18 @@ package cmderr
 
 import "errors"
 
-var NegativeLimitFlagError = errors.New("The --limit flag must be positive.")
-var OutputFormatInvalidError = errors.New("Unknown output format.")
-var WrongIDArgumentFormatError = errors.New("The ID argument must be a number.")
+var NegativeLimitFlagError = errors.New(
+	"The --limit flag must be positive.",
+)
+
+var OutputFormatInvalidError = errors.New(
+	"Unknown output format.",
+)
+
+var WrongIDArgumentFormatError = errors.New(
+	"The ID argument must be a number.",
+)
+
+var InvalidDateFormatError = errors.New(
+	"Please enter a valid date in the format dd/mm/yyyy",
+)
