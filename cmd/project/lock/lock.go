9a86b94f9e7976079cf168ecd117685bebbd7199
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-20 13:05:24 +0100

    feat(project): add lock/unlock commands

---
+++ cmd/project/lock/lock.go
@@ -0,0 +1,46 @@
+package lock
+
+import (
+	"taikun-cli/api"
+	"taikun-cli/apiconfig"
+	"taikun-cli/cmd/cmderr"
+	"taikun-cli/utils/format"
+	"taikun-cli/utils/types"
+
+	"github.com/itera-io/taikungoclient/client/projects"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdLock() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "lock <project-id>",
+		Short: "Lock a project",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			projectID, err := types.Atoi32(args[0])
+			if err != nil {
+				return cmderr.WrongIDArgumentFormatError
+			}
+			return lockRun(projectID)
+		},
+	}
+
+	return cmd
+}
+
+func lockRun(projectID int32) (err error) {
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	params := projects.NewProjectsLockManagerParams().WithV(apiconfig.Version)
+	params = params.WithMode(&types.LockedMode).WithID(&projectID)
+
+	_, err = apiClient.Client.Projects.ProjectsLockManager(params, apiClient)
+	if err == nil {
+		format.PrintStandardSuccess()
+	}
+
+	return
+}
--- cmd/project/project.go
+++ cmd/project/project.go
@@ -4,6 +4,8 @@ import (
 	"taikun-cli/cmd/project/create"
 	"taikun-cli/cmd/project/delete"
 	"taikun-cli/cmd/project/list"
+	"taikun-cli/cmd/project/lock"
+	"taikun-cli/cmd/project/unlock"
 
 	"github.com/spf13/cobra"
 )
@@ -17,6 +19,8 @@ func NewCmdProject() *cobra.Command {
 	cmd.AddCommand(create.NewCmdCreate())
 	cmd.AddCommand(delete.NewCmdDelete())
 	cmd.AddCommand(list.NewCmdList())
+	cmd.AddCommand(lock.NewCmdLock())
+	cmd.AddCommand(unlock.NewCmdUnlock())
 
 	return cmd
 }
