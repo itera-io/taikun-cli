6393947a04c58d4061cd02f278408a0b9adb41f7
Thibaud Deleon <thibaud.deleon@epita.fr>
2021-12-20 12:30:59 +0100

    feat(project): get default access/alerting profile in create

--- cmd/project/create/create.go
+++ cmd/project/create/create.go
@@ -6,7 +6,10 @@ import (
 	"taikun-cli/cmd/cmdutils"
 	"taikun-cli/utils/format"
 
+	"github.com/itera-io/taikungoclient/client/access_profiles"
+	"github.com/itera-io/taikungoclient/client/alerting_profiles"
 	"github.com/itera-io/taikungoclient/client/projects"
+	"github.com/itera-io/taikungoclient/client/users"
 	"github.com/itera-io/taikungoclient/models"
 	"github.com/spf13/cobra"
 )
@@ -42,16 +45,26 @@ func NewCmdCreate() *cobra.Command {
 		},
 	}
 
-	cmd.Flags().Int32Var(
-		&opts.AccessProfileID, "access-profile-id", 0,
-		"Access profile ID",
-	)
-
 	cmd.Flags().Int32VarP(
 		&opts.CloudCredentialID, "cloud-credential-id", "c", 0,
 		"Cloud credential ID (required)",
 	)
 	cmdutils.MarkFlagRequired(cmd, "cloud-credential-id")
+
+	cmd.Flags().Int32Var(
+		&opts.AccessProfileID, "access-profile-id", 0,
+		"Access profile ID",
+	)
+
+	cmd.Flags().Int32Var(
+		&opts.AlertingProfileID, "alerting-profile-id", 0,
+		"Alerting profile ID",
+	)
+
+	cmd.Flags().Int32VarP(
+		&opts.OrganizationID, "organization-id", "o", 0,
+		"Organization ID",
+	)
 
 	return cmd
 }
@@ -62,19 +75,97 @@ func createRun(opts *CreateOptions) (err error) {
 		return
 	}
 
+	if opts.OrganizationID == 0 {
+		opts.OrganizationID, err = getDefaultOrganizationID()
+		if err != nil {
+			return
+		}
+	}
+	if opts.AccessProfileID == 0 {
+		opts.AccessProfileID, err = getDefaultAccessProfileID(opts.OrganizationID)
+		if err != nil {
+			return
+		}
+	}
+	if opts.AlertingProfileID == 0 {
+		opts.AlertingProfileID, err = getDefaultAlertingProfileID(opts.OrganizationID)
+		if err != nil {
+			return
+		}
+	}
+
 	body := models.CreateProjectCommand{
 		AccessProfileID:   opts.AccessProfileID,
+		AlertingProfileID: opts.AlertingProfileID,
 		CloudCredentialID: opts.CloudCredentialID,
 		Name:              opts.Name,
+		OrganizationID:    opts.OrganizationID,
 	}
 
 	params := projects.NewProjectsCreateParams().WithV(apiconfig.Version)
 	params = params.WithBody(&body)
 
-	rep, err := apiClient.Client.Projects.ProjectsCreate(params, apiClient)
+	response, err := apiClient.Client.Projects.ProjectsCreate(params, apiClient)
 	if err == nil {
-		format.PrettyPrintJson(rep.Payload)
+		format.PrettyPrintJson(response.Payload)
 	}
 
+	return
+}
+
+func getDefaultOrganizationID() (id int32, err error) {
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+	params := users.NewUsersDetailsParams().WithV(apiconfig.Version)
+	response, err := apiClient.Client.Users.UsersDetails(params, apiClient)
+	if err == nil {
+		id = response.Payload.Data.OrganizationID
+	}
+	return
+}
+
+func getDefaultAccessProfileID(organizationID int32) (id int32, err error) {
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	params := access_profiles.NewAccessProfilesAccessProfilesForOrganizationListParams()
+	params = params.WithV(apiconfig.Version).WithOrganizationID(&organizationID)
+	response, err := apiClient.Client.AccessProfiles.AccessProfilesAccessProfilesForOrganizationList(params, apiClient)
+	if err != nil {
+		return
+	}
+
+	for _, profile := range response.Payload {
+		if profile.Name == apiconfig.DefaultAccessProfileName {
+			id = profile.ID
+			return
+		}
+	}
+	return
+}
+
+func getDefaultAlertingProfileID(organizationID int32) (id int32, err error) {
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	params := alerting_profiles.NewAlertingProfilesAlertingProfilesForOrganizationListParams()
+	params = params.WithV(apiconfig.Version).WithOrganizationID(&organizationID)
+	response, err := apiClient.Client.AlertingProfiles.AlertingProfilesAlertingProfilesForOrganizationList(params, apiClient)
+	if err != nil {
+		return
+	}
+
+	for _, profile := range response.Payload {
+		if profile.Name == apiconfig.DefaultAlertingProfileName {
+			id = profile.ID
+			return
+		}
+	}
 	return
 }
