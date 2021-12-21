523c601c3c360f3ea7f672b9b1247ab109d39ee1
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-12-21 17:00:44 +0100

    feat(openstack): add --id-only flag & table format to create

--- cmd/cloudcredential/openstack/create/create.go
+++ cmd/cloudcredential/openstack/create/create.go
@@ -4,6 +4,7 @@ import (
 	"github.com/itera-io/taikun-cli/api"
 	"github.com/itera-io/taikun-cli/apiconfig"
 	"github.com/itera-io/taikun-cli/cmd/cmdutils"
+	"github.com/itera-io/taikun-cli/config"
 	"github.com/itera-io/taikun-cli/utils/format"
 
 	"github.com/itera-io/taikungoclient/client/openstack"
@@ -25,6 +26,7 @@ type CreateOptions struct {
 	VolumeType       string
 	ImportNetwork    bool
 	OrganizationID   int32
+	IDOnly           bool
 }
 
 func NewCmdCreate() *cobra.Command {
@@ -68,7 +70,24 @@ func NewCmdCreate() *cobra.Command {
 
 	cmd.Flags().Int32VarP(&opts.OrganizationID, "organization-id", "o", 0, "Organization ID")
 
+	cmdutils.AddIdOnlyFlag(cmd, &opts.IDOnly)
+
 	return cmd
+}
+
+func printResult(resource interface{}) {
+	if config.OutputFormat == config.OutputFormatJson {
+		format.PrettyPrintJson(resource)
+	} else if config.OutputFormat == config.OutputFormatTable {
+		format.PrettyPrintApiResponseTable(resource,
+			"id",
+			"cloudCredentialName",
+			"organizationName",
+			"openStackProject",
+			"openStackUser",
+			"isLocked",
+		)
+	}
 }
 
 func createRun(opts *CreateOptions) (err error) {
@@ -96,7 +115,11 @@ func createRun(opts *CreateOptions) (err error) {
 	params := openstack.NewOpenstackCreateParams().WithV(apiconfig.Version).WithBody(body)
 	response, err := apiClient.Client.Openstack.OpenstackCreate(params, apiClient)
 	if err == nil {
-		format.PrettyPrintJson(response.Payload)
+		if opts.IDOnly {
+			format.PrintResourceID(response.Payload)
+		} else {
+			printResult(response.Payload)
+		}
 	}
 
 	return
