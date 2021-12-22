9b7c297d13a785cf8bf55a5a96d8b15ae8601d5a
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-12-22 12:59:02 +0100

    test(access-profile): test unlock command

---
+++ cmd/accessprofile/unlock/test_spec.sh
@@ -0,0 +1,35 @@
+Context 'accessprofile/unlock'
+  cleanup() {
+    taikun access-profile delete $id
+  }
+
+  BeforeEach 'setup'
+  AfterEach 'cleanup'
+
+  Context 'unlocked access profile'
+    setup() {
+      name=_rnd_name
+      id=$(taikun access-profile create $RSC_PREFIX-$name -I)
+    }
+
+    Example 'unlock an unlocked access profile'
+      When call taikun access-profile unlock $id
+      The stderr should include '400'
+      The status should equal 1
+    End
+  End
+
+  Context 'locked access profile'
+    setup() {
+      name=_rnd_name
+      id=$(taikun access-profile create $RSC_PREFIX-$name -I)
+      taikun access-profile lock $id
+    }
+
+    Example 'unlock a locked access profile'
+      When call taikun access-profile unlock $id
+      The output should equal 'Operation was successful.'
+      The status should equal 0
+    End
+  End
+End
