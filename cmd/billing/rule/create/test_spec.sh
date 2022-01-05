d471b1d338b3a09d3839fd73b8339d3d048ea830
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-05 12:09:46 +0100

    test(billing-rule): test cases for create

--- cmd/billing/rule/create/test_spec.sh
+++ cmd/billing/rule/create/test_spec.sh
@@ -1,6 +1,30 @@
 Context 'billing/rule/create'
 
-  Todo 'create simple billing rule'
-  Todo 'duplicate name'
+  flags="" # FIXME
+
+  setup() {
+    name=$(_rnd_name)
+    id=$(taikun billing rule create $name $flags -I)
+  }
+
+  cleanup() {
+    taikun billing rule delete $id -q 2>/dev/null || true
+  }
+
+  BeforeEach 'setup'
+  AfterEach 'cleanup'
+
+  Example 'create single billing rule'
+    When call taikun billing rule list --no-decorate
+    The status should equal 0
+    The output should include $name
+  End
+
+  Example 'duplicate name causes error'
+    When call taikun billing rule create $name $flags
+    The status should equal 1
+    The stderr should include '400'
+    The stderr should include 'already exists'
+  End
 
 End
