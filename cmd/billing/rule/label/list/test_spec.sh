475d651b68d7b94390b94b4153806dfa1c9e8b17
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-05 17:14:49 +0100

    test(billing-rule): label list test cases

---
+++ cmd/billing/rule/label/list/test_spec.sh
@@ -0,0 +1,38 @@
+Context 'billing/rule/label/list'
+
+  setup() {
+    name=$(_rnd_name)
+    pass=$PROMETHEUS_PASSWORD
+    url=$PROMETHEUS_URL
+    user=$PROMETHEUS_USERNAME
+
+    cid=$(taikun billing cred create $name -p $pass -u $url -l $user -I)
+    id=$(taikun billing rule create $name -b $cid -l ed=vim,lang=rust -m abc --price 1 --price-rate 1 --type count -I)
+  }
+
+  BeforeEach 'setup'
+
+  cleanup() {
+    taikun billing rule delete $id -q 2> /dev/null || true
+    taikun billing credential delete $cid -q 2>/dev/null || true
+  }
+
+  AfterEach 'cleanup'
+
+  Example 'List all labels'
+    When call taikun billing rule label list $id --no-decorate
+    The status should equal 0
+    The lines of output should equal 2
+    The output should include vim
+    The output should include rust
+    The output should include ed
+    The output should include lang
+  End
+
+  Example 'List only one label'
+    When call taikun billing rule label list $id --no-decorate -l 1
+    The status should equal 0
+    The lines of output should equal 1
+  End
+
+End
