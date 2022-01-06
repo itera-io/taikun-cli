bec02a6e7575d5df84ebc85c30a05226a841a180
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-06 12:37:27 +0100

    feat(billing-rule): command to bind an organization

--- cmd/billing/rule/organization/bind/bind.go
+++ cmd/billing/rule/organization/bind/bind.go
@@ -1,39 +1,71 @@
 package bind
 
 import (
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/apiconfig"
 	"github.com/itera-io/taikun-cli/cmd/cmdutils"
+	"github.com/itera-io/taikun-cli/utils/format"
+	"github.com/itera-io/taikun-cli/utils/types"
+	"github.com/itera-io/taikungoclient/client/prometheus"
+	"github.com/itera-io/taikungoclient/models"
 	"github.com/spf13/cobra"
 )
 
 type BindOptions struct {
-	BillingRuleID   int32
-	OrganizationIDs []int32
+	BillingRuleID  int32
+	DiscountRate   float64
+	OrganizationID int32
 }
 
 func NewCmdBind() *cobra.Command {
 	var opts BindOptions
 
 	cmd := cobra.Command{
-		Use:   "bind <organization-id>...",
-		Short: "Bind a billing rule to one or more organizations",
-		Args:  cobra.MinimumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			ids, err := cmdutils.ArgsToNumericalIDs(args)
+		Use:   "bind <billing-rule-id>",
+		Short: "Bind a billing rule to an organization",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			opts.BillingRuleID, err = types.Atoi32(args[0])
 			if err != nil {
 				return err
 			}
-			opts.OrganizationIDs = ids
 			return bindRun(&opts)
 		},
 	}
 
-	cmd.Flags().Int32VarP(&opts.BillingRuleID, "billing-rule-id", "b", 0, "Billing rule ID")
-	cmdutils.MarkFlagRequired(&cmd, "billing-rule-id")
+	cmd.Flags().Float64VarP(&opts.DiscountRate, "discount-rate", "d", 0, "Discount rate (required)")
+	cmdutils.MarkFlagRequired(&cmd, "discount-rate")
+
+	cmd.Flags().Int32VarP(&opts.OrganizationID, "organization-id", "o", 0, "Organization ID (required)")
+	cmdutils.MarkFlagRequired(&cmd, "organization-id")
 
 	return &cmd
 }
 
 func bindRun(opts *BindOptions) (err error) {
-	// FIXME
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	body := models.BindPrometheusOrganizationsCommand{
+		PrometheusRuleID: opts.BillingRuleID,
+		Organizations: []*models.BindOrganizationsToRuleDto{
+			{
+				OrganizationID:   opts.OrganizationID,
+				RuleDiscountRate: opts.DiscountRate,
+				IsBound:          true,
+			},
+		},
+	}
+
+	params := prometheus.NewPrometheusBindOrganizationsParams().WithV(apiconfig.Version)
+	params = params.WithBody(&body)
+
+	_, err = apiClient.Client.Prometheus.PrometheusBindOrganizations(params, apiClient)
+	if err == nil {
+		format.PrintStandardSuccess()
+	}
+
 	return
 }
--- cmd/billing/rule/organization/list/test_spec.sh
+++ cmd/billing/rule/organization/list/test_spec.sh
@@ -6,6 +6,11 @@ Context 'billing/rule/organization/list'
     url=$PROMETHEUS_URL
     user=$PROMETHEUS_USERNAME
 
+    org_name1=$(_rnd_name)
+    oid1=$(taikun organization create $org_name1 --full-name $org_name1 -I)
+    org_name2=$(_rnd_name)
+    oid2=$(taikun organization create $org_name2 --full-name $org_name2 -I)
+
     cid=$(taikun billing cred create $name -p $pass -u $url -l $user -I)
     id=$(taikun billing rule create $name -b $cid -l foo=foo -m abc --price 1 --price-rate 1 --type count -I)
   }
@@ -13,8 +18,10 @@ Context 'billing/rule/organization/list'
   BeforeEach 'setup'
 
   cleanup() {
-    taikun billing rule delete $id -q 2> /dev/null || true
+    taikun billing rule delete $id -q 2>/dev/null || true
     taikun billing credential delete $cid -q 2>/dev/null || true
+    taikun organization delete $oid1 -q 2>/dev/null || true
+    taikun organization delete $oid2 -q 2>/dev/null || true
   }
 
   AfterEach 'cleanup'
@@ -25,6 +32,29 @@ Context 'billing/rule/organization/list'
     The lines of output should equal 0
   End
 
-  Todo 'multiple bindings'
+  Context
+
+    bind_org() {
+      taikun billing rule org bind $id -o $oid1 -d 42 -q
+      taikun billing rule org bind $id -o $oid2 -d 42 -q
+    }
+
+    BeforeEach 'bind_org'
+
+    Example 'list all bindings'
+      When call taikun billing rule org list $id --no-decorate
+      The status should equal 0
+      The lines of output should equal 2
+      The output should include $org_name1
+      The output should include $org_name2
+    End
+
+    Example 'list only one binding'
+      When call taikun billing rule org list $id --no-decorate --limit 1
+      The status should equal 0
+      The lines of output should equal 1
+    End
+
+  End
 
 End
