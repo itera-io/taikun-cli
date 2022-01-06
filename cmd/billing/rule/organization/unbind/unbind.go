f44d884318d45ca15586eb85e34da4f6f0bb8c5f
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-06 12:45:01 +0100

    feat(billing-rule): command to unbind an organization

--- cmd/billing/rule/organization/unbind/unbind.go
+++ cmd/billing/rule/organization/unbind/unbind.go
@@ -1,29 +1,66 @@
 package unbind
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/apiconfig"
+	"github.com/itera-io/taikun-cli/cmd/cmdutils"
+	"github.com/itera-io/taikun-cli/utils/format"
+	"github.com/itera-io/taikun-cli/utils/types"
+	"github.com/itera-io/taikungoclient/client/prometheus"
+	"github.com/itera-io/taikungoclient/models"
+	"github.com/spf13/cobra"
+)
 
 type UnbindOptions struct {
-	// FIXME
+	BillingRuleID  int32
+	OrganizationID int32
 }
 
 func NewCmdUnbind() *cobra.Command {
 	var opts UnbindOptions
 
 	cmd := cobra.Command{
-		Use:   "unbind <organization-id>...",
-		Short: "Unbind a billing rule from one or more organizations",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			// FIXME
+		Use:   "unbind <billing-rule-id>",
+		Short: "Unbind a billing rule from an organization",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			opts.BillingRuleID, err = types.Atoi32(args[0])
+			if err != nil {
+				return err
+			}
 			return unbindRun(&opts)
 		},
 	}
 
-	// FIXME add flags
+	cmd.Flags().Int32VarP(&opts.OrganizationID, "organization-id", "o", 0, "Organization ID (required)")
+	cmdutils.MarkFlagRequired(&cmd, "organization-id")
 
 	return &cmd
 }
 
 func unbindRun(opts *UnbindOptions) (err error) {
-	// FIXME
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	body := models.BindPrometheusOrganizationsCommand{
+		PrometheusRuleID: opts.BillingRuleID,
+		Organizations: []*models.BindOrganizationsToRuleDto{
+			{
+				OrganizationID: opts.OrganizationID,
+				IsBound:        false,
+			},
+		},
+	}
+
+	params := prometheus.NewPrometheusBindOrganizationsParams().WithV(apiconfig.Version)
+	params = params.WithBody(&body)
+
+	_, err = apiClient.Client.Prometheus.PrometheusBindOrganizations(params, apiClient)
+	if err == nil {
+		format.PrintStandardSuccess()
+	}
+
 	return
 }
