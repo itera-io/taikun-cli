ffe3d3a90aba19263f66850769624894dba8a928
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-07 14:32:21 +0100

    refactor: move flavor/{list,bind,unbind} to project/flavor/{list,bind,unbind}

--- cmd/root/root.go
+++ cmd/root/root.go
@@ -10,7 +10,6 @@ import (
 	"github.com/itera-io/taikun-cli/cmd/cloudcredential"
 	"github.com/itera-io/taikun-cli/cmd/cmderr"
 	"github.com/itera-io/taikun-cli/cmd/cmdutils"
-	"github.com/itera-io/taikun-cli/cmd/flavor"
 	"github.com/itera-io/taikun-cli/cmd/kubernetesprofile"
 	"github.com/itera-io/taikun-cli/cmd/organization"
 	"github.com/itera-io/taikun-cli/cmd/policyprofile"
@@ -46,7 +45,6 @@ func NewCmdRoot() *cobra.Command {
 	cmd.AddCommand(backupcredential.NewCmdBackupCredential())
 	cmd.AddCommand(billing.NewCmdBilling())
 	cmd.AddCommand(cloudcredential.NewCmdCloudCredential())
-	cmd.AddCommand(flavor.NewCmdFlavor())
 	cmd.AddCommand(kubernetesprofile.NewCmdKubernetesProfile())
 	cmd.AddCommand(organization.NewCmdOrganization())
 	cmd.AddCommand(policyprofile.NewCmdPolicyProfile())
