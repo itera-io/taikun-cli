0c7cc82914ed8abeab80733d5c33f9f8208c4401
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-10 14:32:38 +0100

    docs: command-tree generation script has variable exit status

--- scripts/docs/build_command_tree.sh
+++ scripts/docs/build_command_tree.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+cmd_tree_path="COMMAND_TREE.md"
+
+# save previous command tree
+if [ -f $cmd_tree_path ]; then
+  mv $cmd_tree_path "${cmd_tree_path}.bak"
+fi
+
 # replace main.go
 cp main.go main.go.bak
 cat > main.go << EOF
@@ -21,7 +28,25 @@ EOF
 go build -o taikun
 
 # write command tree
-./taikun > COMMAND_TREE.md
+./taikun > $cmd_tree_path
 
 # reset main.go
 mv main.go.bak main.go
+
+# exit successfully if the command tree was generated for the first time
+if [ ! -f "${cmd_tree_path}.bak" ]; then
+  echo "Command tree was generated for the first time."
+  exit 0
+fi
+
+# exit with non-zero status if the command tree has *not* changed
+if diff $cmd_tree_path "${cmd_tree_path}.bak" 1>/dev/null; then
+  # remove previous command tree
+  rm "${cmd_tree_path}.bak"
+  echo "Command tree hasn't changed."
+  exit 1
+fi
+
+# remove previous command tree
+rm "${cmd_tree_path}.bak"
+echo "Command tree has changed."
