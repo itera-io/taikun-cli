04c55a0bd39ebde5888c577adb3f812ff7a9ec3b
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-10 16:42:07 +0100

    test(project-backup): add test cases enabling and disabling backup

--- cmd/project/backup/test_spec.sh
+++ cmd/project/backup/test_spec.sh
@@ -1,10 +1,9 @@
 Context 'project/backup'
 
   setup() {
-    bid=$(taikun backup-credential create foo -a $AWS_ACCESS_KEY_ID -e $S3_ENDPOINT -r $S3_REGION -s $AWS_SECRET_ACCESS_KEY -I)
+    bid=$(taikun backup-credential create $(_rnd_name) -a $AWS_ACCESS_KEY_ID -e $S3_ENDPOINT -r $S3_REGION -s $AWS_SECRET_ACCESS_KEY -I)
     ccid=$(taikun cloud-credential openstack create $(_rnd_name) -d $OS_USER_DOMAIN_NAME -p $OS_PASSWORD --project $OS_PROJECT_NAME -r $OS_REGION_NAME -u $OS_USERNAME --public-network $OS_INTERFACE --url $OS_AUTH_URL -I)
-    flavor=$(taikun cloud-credential flavors $ccid --no-decorate --min-cpu 4 --max-cpu 4 --min-ram 8 --max-ram 8 -C name --limit 1)
-    pid=$(taikun project create $(_rnd_name) --cloud-credential-id $ccid --flavors $flavor -I)
+    pid=$(taikun project create $(_rnd_name) --cloud-credential-id $ccid -I)
   }
 
   BeforeEach 'setup'
@@ -19,4 +18,33 @@ Context 'project/backup'
 
   AfterEach 'cleanup'
 
+  get_backup_status() {
+    taikun project info $1 --no-decorate | grep -i backup | tr -d ' ' -f 2
+  }
+
+  Context
+    enable_backup() {
+      taikun project backup enable $pid -b $bid -q
+    }
+    Before 'enable_backup'
+
+    Example 'enable backup'
+      When call get_backup_status $pid
+      The status should equal 0
+      The output should equal true
+    End
+  End
+
+  Context
+    disable_backup() {
+      taikun project backup disable $pid -q
+    }
+    Before 'disable_backup'
+
+    Example 'disable backup'
+      When call get_backup_status $pid
+      The status should equal 0
+      The output should equal false
+    End
+  End
 End
