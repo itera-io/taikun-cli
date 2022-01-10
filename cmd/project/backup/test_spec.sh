5fff7c043eb4de616c53b655140a373fcaac7aab
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-10 15:34:54 +0100

    test(project-backup): setup and cleanup for tests

--- cmd/project/backup/test_spec.sh
+++ cmd/project/backup/test_spec.sh
@@ -1,5 +1,22 @@
 Context 'project/backup'
 
-  Todo 'test enabling/disabling backup'
+  setup() {
+    bid=$(taikun backup-credential create foo -a $AWS_ACCESS_KEY_ID -e $S3_ENDPOINT -r $S3_REGION -s $AWS_SECRET_ACCESS_KEY -I)
+    ccid=$(taikun cloud-credential openstack create $(_rnd_name) -d $OS_USER_DOMAIN_NAME -p $OS_PASSWORD --project $OS_PROJECT_NAME -r $OS_REGION_NAME -u $OS_USERNAME --public-network $OS_INTERFACE --url $OS_AUTH_URL -I)
+    flavor=$(taikun cloud-credential flavors $ccid --no-decorate --min-cpu 4 --max-cpu 4 --min-ram 8 --max-ram 8 -C name --limit 1)
+    pid=$(taikun project create $(_rnd_name) --cloud-credential-id $ccid --flavors $flavor -I)
+  }
+
+  BeforeEach 'setup'
+
+  cleanup() {
+    if ! taikun project delete $pid -q 2>/dev/null; then
+      taikun project delete --force $pid -q 2>/dev/null || true
+    fi
+    taikun cloud-credential delete $ccid -q 2>/dev/null || true
+    taikun backup-credential delete $bid -q 2>/dev/null || true
+  }
+
+  AfterEach 'cleanup'
 
 End
