afe3789b375125b388e4897a4b16447cd0d9826c
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-10 13:40:59 +0100

    refactor: redefine enum-types using new generic map

--- utils/types/showback.go
+++ utils/types/showback.go
@@ -3,14 +3,17 @@ package types
 import (
 	"strings"
 
+	"github.com/itera-io/taikun-cli/utils/gmap"
 	"github.com/itera-io/taikungoclient/models"
 )
 
-var ShowbackKinds = map[string]interface{}{
-	"general":  models.ShowbackType(100),
-	"external": models.ShowbackType(200),
-}
+var ShowbackKinds = gmap.New(
+	map[string]interface{}{
+		"general":  models.ShowbackType(100),
+		"external": models.ShowbackType(200),
+	},
+)
 
 func GetShowbackKind(showbackKind string) models.ShowbackType {
-	return ShowbackKinds[strings.ToLower(showbackKind)].(models.ShowbackType)
+	return ShowbackKinds.Get(strings.ToLower(showbackKind)).(models.ShowbackType)
 }
