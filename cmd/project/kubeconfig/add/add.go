072be109d6fd7d53b884e379933a0285b2296f72
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-12 14:58:23 +0100

    feat(project-kubeconfig): add

--- cmd/project/kubeconfig/add/add.go
+++ cmd/project/kubeconfig/add/add.go
@@ -1,36 +1,94 @@
 package add
 
 import (
-	"log"
+	"fmt"
 
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/apiconfig"
+	"github.com/itera-io/taikun-cli/cmd/cmdutils"
+	"github.com/itera-io/taikun-cli/utils/format"
+	"github.com/itera-io/taikun-cli/utils/types"
+	"github.com/itera-io/taikungoclient/client/kube_config"
+	"github.com/itera-io/taikungoclient/models"
 	"github.com/spf13/cobra"
 )
 
 type AddOptions struct {
-	ProjectID int32
-	// FIXME add options
+	AccessScope string
+	ProjectID   int32
+	Name        string
+	Role        string
 }
 
 func NewCmdAdd() *cobra.Command {
 	var opts AddOptions
 
 	cmd := cobra.Command{
-		Use:   "add <project-id>",
+		Use:   "add <name>",
 		Short: "Add a kubeconfig",
-		Args:  cobra.NoArgs, // FIXME maybe
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// FIXME maybe
+			opts.Name = args[0]
+			if err := cmdutils.CheckFlagValue("role", opts.Role, types.KubeconfigRoles); err != nil {
+				return err
+			}
+			if err := cmdutils.CheckFlagValue("access-scope", opts.AccessScope, types.KubeconfigAccessScopes); err != nil {
+				return err
+			}
 			return addRun(&opts)
 		},
 	}
 
-	// FIXME
+	cmd.Flags().StringVarP(&opts.AccessScope, "access-scope", "a", "",
+		fmt.Sprintf(
+			"Who can use the kubeconfig: `%s` (%s), `%s` (%s) or `%s` (%s) (required)",
+			types.KubeconfigAccessPersonal, "only you",
+			types.KubeconfigAccessManagers, "managers only",
+			types.KubeconfigAccessAll, "all users with access to this project",
+		),
+	)
+	cmdutils.MarkFlagRequired(&cmd, "access-scope")
+	cmdutils.RegisterStaticFlagCompletion(&cmd, "access-scope", types.KubeconfigAccessScopes.Keys()...)
+
+	cmd.Flags().Int32VarP(&opts.ProjectID, "project-id", "p", 0, "Project ID (required)")
+	cmdutils.MarkFlagRequired(&cmd, "project-id")
+
+	cmd.Flags().StringVarP(&opts.Role, "role", "r", "", "Role (required)")
+	cmdutils.MarkFlagRequired(&cmd, "role")
+	cmdutils.RegisterStaticFlagCompletion(&cmd, "role", types.KubeconfigRoles.Keys()...)
 
 	return &cmd
 }
 
 func addRun(opts *AddOptions) (err error) {
-	// FIXME
-	log.Println("TODO")
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	body := models.CreateKubeConfigCommand{
+		IsAccessibleForAll:     opts.AccessScope == types.KubeconfigAccessAll,
+		IsAccessibleForManager: opts.AccessScope == types.KubeconfigAccessManagers,
+		KubeConfigRoleID:       types.GetKubeconfigRole(opts.Role),
+		Name:                   opts.Name,
+		ProjectID:              opts.ProjectID,
+	}
+
+	params := kube_config.NewKubeConfigCreateParams().WithV(apiconfig.Version)
+	params = params.WithBody(&body)
+
+	response, err := apiClient.Client.KubeConfig.KubeConfigCreate(params, apiClient)
+	if err == nil {
+		format.PrintResult(response.Payload,
+			"id",
+			"serviceAccountName",
+			"userName",
+			"userRole",
+			"projectName",
+			"isAccessibleForAll",
+			"kubeConfigRoleName",
+		)
+	}
+
 	return
 }
---
+++ utils/types/kubeconfig.go
@@ -0,0 +1,30 @@
+package types
+
+import "github.com/itera-io/taikun-cli/utils/gmap"
+
+var KubeconfigRoles = gmap.New(
+	map[string]interface{}{
+		"cluster-admin": int32(1),
+		"admin":         int32(2),
+		"edit":          int32(3),
+		"view":          int32(4),
+	},
+)
+
+func GetKubeconfigRole(role string) int32 {
+	return KubeconfigRoles.Get(role).(int32)
+}
+
+const (
+	KubeconfigAccessPersonal = "personal"
+	KubeconfigAccessManagers = "managers"
+	KubeconfigAccessAll      = "all"
+)
+
+var KubeconfigAccessScopes = gmap.New(
+	map[string]interface{}{
+		KubeconfigAccessPersonal: struct{}{},
+		KubeconfigAccessManagers: struct{}{},
+		KubeconfigAccessAll:      struct{}{},
+	},
+)
