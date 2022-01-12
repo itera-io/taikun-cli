29aede0c6fa988d986d139c6c35a058d0d58cd71
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-12 14:58:14 +0100

    feat(project-kubeconfig): list

--- cmd/project/kubeconfig/list/list.go
+++ cmd/project/kubeconfig/list/list.go
@@ -1,8 +1,13 @@
 package list
 
 import (
-	"log"
-
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/apiconfig"
+	"github.com/itera-io/taikun-cli/cmd/cmdutils"
+	"github.com/itera-io/taikun-cli/utils/format"
+	"github.com/itera-io/taikun-cli/utils/types"
+	"github.com/itera-io/taikungoclient/client/kube_config"
+	"github.com/itera-io/taikungoclient/models"
 	"github.com/spf13/cobra"
 )
 
@@ -16,20 +21,44 @@ func NewCmdList() *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "list <project-id>",
 		Short: "List a project's kubeconfigs",
-		Args:  cobra.NoArgs, // FIXME maybe
-		RunE: func(cmd *cobra.Command, args []string) error {
-			// FIXME maybe
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			opts.ProjectID, err = types.Atoi32(args[0])
+			if err != nil {
+				return
+			}
 			return listRun(&opts)
 		},
+		Aliases: cmdutils.ListAliases,
 	}
 
-	// FIXME
+	cmdutils.AddSortByAndReverseFlags(&cmd, models.KubeConfigForUserDto{})
 
 	return &cmd
 }
 
 func listRun(opts *ListOptions) (err error) {
-	// FIXME
-	log.Println("TODO")
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	params := kube_config.NewKubeConfigListParams().WithV(apiconfig.Version)
+	params = params.WithProjectID(&opts.ProjectID)
+
+	response, err := apiClient.Client.KubeConfig.KubeConfigList(params, apiClient)
+	if err == nil {
+		format.PrintResults(response.Payload.Data,
+			"id",
+			"serviceAccountName",
+			"userName",
+			"userRole",
+			"projectName",
+			"isAccessibleForAll",
+			"kubeConfigRoleName",
+			"createdBy",
+		)
+	}
+
 	return
 }
