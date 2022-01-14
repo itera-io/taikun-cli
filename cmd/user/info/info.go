0d0fb5e410ef3eb3fb812924b289d03d25ac750e
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-14 11:52:28 +0100

    feat(user): optional arg to get info about other users

--- cmd/user/info/info.go
+++ cmd/user/info/info.go
@@ -2,6 +2,7 @@ package info
 
 import (
 	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/cmd/cmderr"
 	"github.com/itera-io/taikun-cli/utils/out"
 	"github.com/itera-io/taikungoclient/client/users"
 	"github.com/spf13/cobra"
@@ -9,43 +10,54 @@ import (
 
 func NewCmdInfo() *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "info",
-		Short: "Retrieve your information",
-		Args:  cobra.NoArgs,
+		Use:   "info [user-id]",
+		Short: "Retrieve a user's information",
+		Long:  "Retrieve a user's information (yours if no user ID is specified)",
+		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return infoRun()
+			if len(args) == 1 {
+				return infoRun(args[0])
+			}
+			return myInfoRun()
 		},
 	}
 
 	return &cmd
 }
 
-func infoRun() (err error) {
+func myInfoRun() (err error) {
 	apiClient, err := api.NewClient()
 	if err != nil {
 		return
 	}
 
 	params := users.NewUsersDetailsParams().WithV(api.Version)
-
 	response, err := apiClient.Client.Users.UsersDetails(params, apiClient)
 	if err == nil {
-		out.PrintResult(response.Payload.Data,
-			"id",
-			"username",
-			"role",
-			"organizationName",
-			"email",
-			"displayName",
-			"isEmailConfirmed",
-			"isEmailNotificationEnabled",
-			"isApprovedByPartner",
-			"owner",
-			"isLocked",
-			"createdAt",
-			"lastLoginAt",
-		)
+		out.PrintResult(response.Payload.Data)
 	}
+
+	return
+}
+
+func infoRun(userID string) (err error) {
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	params := users.NewUsersListParams().WithV(api.Version)
+	params = params.WithID(&userID)
+
+	response, err := apiClient.Client.Users.UsersList(params, apiClient)
+	if err != nil {
+		return
+	}
+	if len(response.Payload.Data) != 1 {
+		return cmderr.ResourceNotFoundError("User", userID)
+	}
+
+	out.PrintResult(response.Payload.Data[0])
 
 	return
 }
