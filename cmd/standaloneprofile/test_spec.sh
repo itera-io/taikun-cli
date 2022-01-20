ac1387ded135c156564f82136d3746590436738e
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-20 13:27:31 +0100

    test(standalone-profile): define test cases

--- cmd/standaloneprofile/test_spec.sh
+++ cmd/standaloneprofile/test_spec.sh
@@ -1,5 +1,82 @@
 Context 'cmd/standaloneprofile'
 
-  Todo 'TEST'
+  setup() {
+    oid=$(taikun organization add $(_rnd_name) --full-name $(_rnd_name) -I)
 
+    name=$(_rnd_name)
+    pubkey="ssh-ed25519 AAAAxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/xxxxxxxxxxxxxxxxxxxx dummy"
+    id=$(taikun standalone-profile add $name --public-key $pubkey -o $oid -I)
+  }
+  BeforeAll 'setup'
+
+  cleanup() {
+    taikun standalone-profile delete $id -q
+    taikun organization delete $oid -q
+  }
+  AfterAll 'cleanup'
+
+  Example 'add, list and delete'
+    When call taikun standalone-profile list -o $oid --no-decorate --show-large-values
+    The status should equal 0
+    The lines of output should equal 1
+    The output should include "$name"
+    The output should include "$pubkey"
+  End
+
+  Example 'duplicate name should cause error'
+    When call taikun standalone-profile add $name --public-key $pubkey -o $oid
+    The status should equal 1
+    The stderr should include 'duplicate'
+  End
+
+  Example 'calling add without name should cause error'
+    When call taikun standalone-profile add --public-key $pubkey -o $oid
+    The status should equal 1
+    The stderr should equal 'Error: accepts 1 arg(s), received 0'
+  End
+
+  Example 'calling add without public key should cause error'
+    When call taikun standalone-profile add $name -o $oid
+    The status should equal 1
+    The stderr should equal 'Error: required flag(s) "public-key" not set'
+  End
+
+  Context
+    lock() {
+      taikun standalone-profile lock $id -q
+    }
+    BeforeEach 'lock'
+
+    unlock() {
+      taikun standalone-profile unlock $id -q
+    }
+    AfterEach 'unlock'
+
+    Example 'lock then unlock'
+      When call taikun standalone-profile list -o $oid --columns lock --no-decorate
+      The status should equal 0
+    The lines of output should equal 1
+      The output should include 'Locked'
+    End
+  End
+
+  Context 'rename'
+    give_new_name() {
+      new_name=$(_rnd_name)
+      taikun standalone-profile rename $id --name $new_name -q
+    }
+    BeforeEach 'give_new_name'
+
+    restore_old_name() {
+      taikun standalone-profile rename $id --name $name -q
+    }
+    AfterEach 'restore_old_name'
+
+    Example 'rename then restore old name'
+      When call taikun standalone-profile list -o $oid --columns name --no-decorate
+      The status should equal 0
+      The lines of output should equal 1
+      The output should include "$name"
+    End
+  End
 End
