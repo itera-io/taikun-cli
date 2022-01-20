17b3e605678423d31b8abcb82c6a9723435ac45e
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-20 14:52:18 +0100

    feat(standalone-profile): implement 'lock', 'unlock' commands

--- cmd/standaloneprofile/lock/lock.go
+++ cmd/standaloneprofile/lock/lock.go
@@ -1,9 +1,17 @@
 package lock
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/cmd/cmderr"
+	"github.com/itera-io/taikun-cli/utils/out"
+	"github.com/itera-io/taikun-cli/utils/types"
+	"github.com/itera-io/taikungoclient/client/stand_alone_profile"
+	"github.com/itera-io/taikungoclient/models"
+	"github.com/spf13/cobra"
+)
 
 type LockOptions struct {
-	// FIXME add options
+	ID int32
 }
 
 func NewCmdLock() *cobra.Command {
@@ -12,19 +20,37 @@ func NewCmdLock() *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "lock <standalone-profile-id>",
 		Short: "Lock a standalone profile",
-		Args:  cobra.ExactArgs(1), // FIXME maybe
-		RunE: func(cmd *cobra.Command, args []string) error {
-			// FIXME maybe
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			opts.ID, err = types.Atoi32(args[0])
+			if err != nil {
+				return cmderr.IDArgumentNotANumberError
+			}
 			return lockRun(&opts)
 		},
 	}
-
-	// FIXME
 
 	return &cmd
 }
 
 func lockRun(opts *LockOptions) (err error) {
-	// FIXME
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	body := models.StandAloneProfileLockManagementCommand{
+		ID:   opts.ID,
+		Mode: types.LockedMode,
+	}
+
+	params := stand_alone_profile.NewStandAloneProfileLockManagementParams().WithV(api.Version)
+	params = params.WithBody(&body)
+
+	_, err = apiClient.Client.StandAloneProfile.StandAloneProfileLockManagement(params, apiClient)
+	if err == nil {
+		out.PrintStandardSuccess()
+	}
+
 	return
 }
--- cmd/standaloneprofile/unlock/unlock.go
+++ cmd/standaloneprofile/unlock/unlock.go
@@ -1,9 +1,17 @@
 package unlock
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/cmd/cmderr"
+	"github.com/itera-io/taikun-cli/utils/out"
+	"github.com/itera-io/taikun-cli/utils/types"
+	"github.com/itera-io/taikungoclient/client/stand_alone_profile"
+	"github.com/itera-io/taikungoclient/models"
+	"github.com/spf13/cobra"
+)
 
 type UnlockOptions struct {
-	// FIXME add options
+	ID int32
 }
 
 func NewCmdUnlock() *cobra.Command {
@@ -13,18 +21,36 @@ func NewCmdUnlock() *cobra.Command {
 		Use:   "unlock <standalone-profile-id>",
 		Short: "Unlock a standalone profile",
 		Args:  cobra.ExactArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			// FIXME maybe
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			opts.ID, err = types.Atoi32(args[0])
+			if err != nil {
+				return cmderr.IDArgumentNotANumberError
+			}
 			return unlockRun(&opts)
 		},
 	}
-
-	// FIXME
 
 	return &cmd
 }
 
 func unlockRun(opts *UnlockOptions) (err error) {
-	// FIXME
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	body := models.StandAloneProfileLockManagementCommand{
+		ID:   opts.ID,
+		Mode: types.UnlockedMode,
+	}
+
+	params := stand_alone_profile.NewStandAloneProfileLockManagementParams().WithV(api.Version)
+	params = params.WithBody(&body)
+
+	_, err = apiClient.Client.StandAloneProfile.StandAloneProfileLockManagement(params, apiClient)
+	if err == nil {
+		out.PrintStandardSuccess()
+	}
+
 	return
 }
