6c26ee146558c2f7be52bddec0b4c4b1b2e5a80f
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-20 14:32:04 +0100

    feat(standalone-profile): implement 'delete' command

--- cmd/standaloneprofile/delete/delete.go
+++ cmd/standaloneprofile/delete/delete.go
@@ -1,34 +1,47 @@
 package delete
 
 import (
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/cmd/cmderr"
 	"github.com/itera-io/taikun-cli/cmd/cmdutils"
+	"github.com/itera-io/taikun-cli/utils/out"
+	"github.com/itera-io/taikungoclient/client/stand_alone_profile"
+	"github.com/itera-io/taikungoclient/models"
 	"github.com/spf13/cobra"
 )
 
-type DeleteOptions struct {
-	// FIXME add options
-}
-
 func NewCmdDelete() *cobra.Command {
-	var opts DeleteOptions
-
 	cmd := cobra.Command{
 		Use:   "delete <standalone-profile-id>...",
 		Short: "Delete one or more standalone profiles",
 		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// FIXME maybe
-			return deleteRun(&opts)
+			ids, err := cmdutils.ArgsToNumericalIDs(args)
+			if err != nil {
+				return cmderr.IDArgumentNotANumberError
+			}
+			return cmdutils.DeleteMultiple(ids, deleteRun)
 		},
 		Aliases: cmdutils.DeleteAliases,
 	}
 
-	// FIXME
-
 	return &cmd
 }
 
-func deleteRun(opts *DeleteOptions) (err error) {
-	// FIXME
+func deleteRun(id int32) (err error) {
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	body := models.DeleteStandAloneProfileCommand{ID: id}
+	params := stand_alone_profile.NewStandAloneProfileDeleteParams().WithV(api.Version)
+	params = params.WithBody(&body)
+
+	_, err = apiClient.Client.StandAloneProfile.StandAloneProfileDelete(params, apiClient)
+	if err == nil {
+		out.PrintDeleteSuccess("Standalone profile", id)
+	}
+
 	return
 }
