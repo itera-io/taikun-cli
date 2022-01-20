079a07b1d41d2c18ccee6a57c7467ef189e21388
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-20 14:10:16 +0100

    feat(standalone-profile): implement 'list' command

--- cmd/standaloneprofile/list/list.go
+++ cmd/standaloneprofile/list/list.go
@@ -1,20 +1,42 @@
 package list
 
 import (
+	"github.com/itera-io/taikun-cli/api"
 	"github.com/itera-io/taikun-cli/cmd/cmdutils"
+	"github.com/itera-io/taikun-cli/config"
+	"github.com/itera-io/taikun-cli/utils/out"
 	"github.com/itera-io/taikun-cli/utils/out/field"
 	"github.com/itera-io/taikun-cli/utils/out/fields"
+	"github.com/itera-io/taikungoclient/client/stand_alone_profile"
+	"github.com/itera-io/taikungoclient/models"
 	"github.com/spf13/cobra"
 )
 
 var listFields = fields.New(
 	[]*field.Field{
-		// FIXME
+		field.NewVisible(
+			"ID", "id",
+		),
+		field.NewVisible(
+			"NAME", "name",
+		),
+		field.NewVisible(
+			"ORG", "organizationName",
+		),
+		field.NewHidden(
+			"ORG-ID", "organizationId",
+		),
+		field.NewVisible(
+			"PUBLIC-KEY", "publicKey",
+		),
+		field.NewVisibleWithToStringFunc(
+			"LOCK", "isLocked", out.FormatLockStatus,
+		),
 	},
 )
 
 type ListOptions struct {
-	// FIXME add options
+	OrganizationID int32
 }
 
 func NewCmdList() *cobra.Command {
@@ -23,20 +45,58 @@ func NewCmdList() *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "list",
 		Short: "List standalone profiles",
-		Args:  cobra.NoArgs, // FIXME maybe
+		Args:  cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// FIXME maybe
 			return listRun(&opts)
 		},
 		Aliases: cmdutils.ListAliases,
 	}
 
-	// FIXME
+	cmd.Flags().Int32VarP(&opts.OrganizationID, "organization-id", "o", 0, "Organization ID (only applies for Partner role)")
+
+	cmdutils.AddLimitFlag(&cmd)
+	cmdutils.AddColumnsFlag(&cmd, listFields)
+	cmdutils.AddSortByAndReverseFlags(&cmd, "standalone-profiles", listFields)
 
 	return &cmd
 }
 
 func listRun(opts *ListOptions) (err error) {
-	// FIXME
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	params := stand_alone_profile.NewStandAloneProfileListParams().WithV(api.Version)
+	if opts.OrganizationID != 0 {
+		params = params.WithOrganizationID(&opts.OrganizationID)
+	}
+	if config.SortBy != "" {
+		params = params.WithSortBy(&config.SortBy).WithSortDirection(api.GetSortDirection())
+	}
+
+	standAloneProfiles := make([]*models.StandAloneProfilesListDto, 0)
+	for {
+		response, err := apiClient.Client.StandAloneProfile.StandAloneProfileList(params, apiClient)
+		if err != nil {
+			return err
+		}
+		standAloneProfiles = append(standAloneProfiles, response.Payload.Data...)
+		count := int32(len(standAloneProfiles))
+		if config.Limit != 0 && count >= config.Limit {
+			break
+		}
+		if count == response.Payload.TotalCount {
+			break
+		}
+		params = params.WithOffset(&count)
+	}
+
+	if config.Limit != 0 && int32(len(standAloneProfiles)) > config.Limit {
+		standAloneProfiles = standAloneProfiles[:config.Limit]
+	}
+
+	out.PrintResults(standAloneProfiles, listFields)
+
 	return
 }
