23b231ae9c5e0f409dd7e3df135bc44d6c615216
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-01-20 09:52:08 +0100

    test(billing-rule): remove failsafes around cleanup func

--- cmd/billing/rule/label/list/test_spec.sh
+++ cmd/billing/rule/label/list/test_spec.sh
@@ -12,8 +12,8 @@ Context 'billing/rule/label/list'
   BeforeAll 'setup'
 
   cleanup() {
-    taikun billing rule delete $id -q 2> /dev/null || true
-    taikun billing credential delete $cid -q 2>/dev/null || true
+    taikun billing rule delete $id -q
+    taikun billing credential delete $cid -q
   }
   AfterAll 'cleanup'
 
