3808f3ea9125755a6110159c5603404106daef24
Thibaud Deleon <thibaud.deleon@epita.fr>
2022-01-24 17:04:02 +0100

    feat(azure): add 'publishers', 'offers', 'skus' list commands

---
+++ cmd/cloudcredential/azure/skus/skus.go
@@ -0,0 +1,114 @@
+package skus
+
+import (
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/cmd/cloudcredential/azure/offers"
+	"github.com/itera-io/taikun-cli/cmd/cloudcredential/azure/publishers"
+	"github.com/itera-io/taikun-cli/cmd/cmdutils"
+	"github.com/itera-io/taikun-cli/utils/out"
+	"github.com/itera-io/taikun-cli/utils/types"
+	"github.com/itera-io/taikungoclient/client/azure"
+	"github.com/spf13/cobra"
+)
+
+type SKUsOptions struct {
+	CloudCredentialID int32
+	Publisher         string
+	Offer             string
+}
+
+func NewCmdSKUs() *cobra.Command {
+	var opts SKUsOptions
+
+	cmd := cobra.Command{
+		Use:   "skus <cloud-credential-id>",
+		Short: "List the image SKUs of an Azure cloud credential",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			opts.CloudCredentialID, err = types.Atoi32(args[0])
+			if err != nil {
+				return
+			}
+			return skusRun(&opts)
+		},
+	}
+
+	cmd.Flags().StringVarP(&opts.Publisher, "publisher", "p", "", "Image publisher (required)")
+	cmdutils.MarkFlagRequired(&cmd, "publisher")
+	cmdutils.SetFlagCompletionFunc(&cmd, "publisher",
+		func(cmd *cobra.Command, args []string, toComplete string) (completions []string) {
+			completions = make([]string, 0)
+
+			if len(args) >= 1 {
+				cloudCredentialID, err := types.Atoi32(args[0])
+				if err == nil {
+					opts := publishers.PublishersOptions{
+						CloudCredentialID: cloudCredentialID,
+					}
+					completions, _ = publishers.ListPublishers(&opts)
+				}
+			}
+
+			return
+		},
+	)
+
+	cmd.Flags().StringVarP(&opts.Offer, "offer", "o", "", "Image offer (required)")
+	cmdutils.MarkFlagRequired(&cmd, "offer")
+	cmdutils.SetFlagCompletionFunc(&cmd, "offer",
+		func(cmd *cobra.Command, args []string, toComplete string) (completions []string) {
+			completions = make([]string, 0)
+
+			if len(args) >= 1 && opts.Publisher != "" {
+				cloudCredentialID, err := types.Atoi32(args[0])
+				if err == nil {
+					opts := offers.OffersOptions{
+						CloudCredentialID: cloudCredentialID,
+						Publisher:         opts.Publisher,
+					}
+					completions, _ = offers.ListOffers(&opts)
+				}
+			}
+
+			return
+		},
+	)
+
+	return &cmd
+}
+
+func skusRun(opts *SKUsOptions) (err error) {
+	skus, err := ListSKUs(opts)
+	if err == nil {
+		out.PrintStringSlice(skus)
+	}
+	return
+}
+
+func ListSKUs(opts *SKUsOptions) (skus []string, err error) {
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	params := azure.NewAzureSkusParams().WithV(api.Version)
+	params = params.WithCloudID(opts.CloudCredentialID)
+	params = params.WithPublisher(opts.Publisher)
+	params = params.WithOffer(opts.Offer)
+
+	skus = make([]string, 0)
+	for {
+		response, err := apiClient.Client.Azure.AzureSkus(params, apiClient)
+		if err != nil {
+			return nil, err
+		}
+		skus = append(skus, response.Payload.Data...)
+		count := int32(len(skus))
+		if count == response.Payload.TotalCount {
+			break
+		}
+		params = params.WithOffset(&count)
+	}
+
+	return
+}
