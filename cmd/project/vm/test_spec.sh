0d197803122518e92556fe06b8850c67ac46de6c
Thibaud Deleon <thibaud.deleon@epita.fr>
2022-01-25 16:20:03 +0100

    test(vm): fix vm add in setup

--- cmd/project/vm/test_spec.sh
+++ cmd/project/vm/test_spec.sh
@@ -3,10 +3,10 @@ Context 'project/vm'
     cc=$(taikun cloud-credential openstack add $(_rnd_name) -d $OS_USER_DOMAIN_NAME -p $OS_PASSWORD --project $OS_PROJECT_NAME -r $OS_REGION_NAME -u $OS_USERNAME --public-network $OS_INTERFACE --url $OS_AUTH_URL -I)
     flavor=$(taikun cloud-credential flavors $cc --no-decorate --min-cpu 4 --max-cpu 4 --min-ram 8 --max-ram 8 -C name --limit 1)
     id=$(taikun project add $(_rnd_name) --cloud-credential-id $cc --flavors $flavor -I)
-    img=$(taikun cc images $cc --columns id --limit 1 --no-decorate)
+    img=$(taikun cc images $cc --no-decorate | egrep -i '(ubuntu)|(focal)' | head -1 | cut -d ' ' -f 1)
     profile=$(taikun standalone-profile add $(_rnd_name) --public-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHshx25CJGDd0HfOQqNt65n/970dsPt0y12lfKKO9fAs dummy" -I)
     taikun project image bind $id --image-ids $img -q
-    vm=$(taikun project vm add $id --name $(_rnd_name) --flavor $flavor --image-id $img --volume-size 5 --profile $profile --tags foo=bar -I)
+    vm=$(taikun project vm add $id --name $(_rnd_name) --flavor $flavor --image-id $img --volume-size 5 --standalone-profile-id $profile --tags foo=bar -I)
   }
   BeforeAll 'setup'
 
