6bf2857ed23425c283d3bd797d4581268dd8a62d
Thibaud Deleon <thibaud.deleon@epita.fr>
2022-01-25 13:04:32 +0100

    feat(project): implement 'image unbind' command

--- cmd/project/image/unbind/unbind.go
+++ cmd/project/image/unbind/unbind.go
@@ -1,35 +1,55 @@
 package unbind
 
 import (
-	"fmt"
-
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/cmd/cmderr"
+	"github.com/itera-io/taikun-cli/cmd/cmdutils"
+	"github.com/itera-io/taikun-cli/utils/out"
+	"github.com/itera-io/taikungoclient/client/images"
+	"github.com/itera-io/taikungoclient/models"
 	"github.com/spf13/cobra"
 )
 
 type UnbindOptions struct {
-	// FIXME
+	ImageBindingIDs []int32
 }
 
 func NewCmdUnbind() *cobra.Command {
 	var opts UnbindOptions
 
 	cmd := cobra.Command{
-		Use:   "unbind <image-bound-id>...",
+		Use:   "unbind <image-binding-id>...",
 		Short: "Unbind one or more images from a project",
 		Args:  cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			// FIXME
+			opts.ImageBindingIDs, err = cmdutils.ArgsToNumericalIDs(args)
+			if err != nil {
+				return cmderr.IDArgumentNotANumberError
+			}
 			return unbindRun(&opts)
 		},
 	}
-
-	// FIXME
 
 	return &cmd
 }
 
 func unbindRun(opts *UnbindOptions) (err error) {
-	// FIXME
-	fmt.Println("TODO")
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	body := models.DeleteImageFromProjectCommand{
+		Ids: opts.ImageBindingIDs,
+	}
+
+	params := images.NewImagesUnbindImagesFromProjectParams().WithV(api.Version)
+	params = params.WithBody(&body)
+
+	_, err = apiClient.Client.Images.ImagesUnbindImagesFromProject(params, apiClient)
+	if err == nil {
+		out.PrintStandardSuccess()
+	}
+
 	return
 }
