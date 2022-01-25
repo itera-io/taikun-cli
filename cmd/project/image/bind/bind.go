44ee7c0c5ce1813ba60eea645c45a94ae1ca5b78
Thibaud Deleon <thibaud.deleon@epita.fr>
2022-01-25 12:51:03 +0100

    feat(project): implement 'image bind' command

--- cmd/project/image/bind/bind.go
+++ cmd/project/image/bind/bind.go
@@ -1,13 +1,18 @@
 package bind
 
 import (
-	"fmt"
-
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/cmd/cmdutils"
+	"github.com/itera-io/taikun-cli/utils/out"
+	"github.com/itera-io/taikun-cli/utils/types"
+	"github.com/itera-io/taikungoclient/client/images"
+	"github.com/itera-io/taikungoclient/models"
 	"github.com/spf13/cobra"
 )
 
 type BindOptions struct {
-	// FIXME
+	ProjectID int32
+	ImageIDs  []string
 }
 
 func NewCmdBind() *cobra.Command {
@@ -18,18 +23,38 @@ func NewCmdBind() *cobra.Command {
 		Short: "Bind one or multiple images to a project",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			// FIXME
+			opts.ProjectID, err = types.Atoi32(args[0])
+			if err != nil {
+				return
+			}
 			return bindRun(&opts)
 		},
 	}
 
-	// FIXME
+	cmd.Flags().StringSliceVarP(&opts.ImageIDs, "image-ids", "i", []string{}, "IDs of the images to bind (required)")
+	cmdutils.MarkFlagRequired(&cmd, "image-ids")
 
 	return &cmd
 }
 
 func bindRun(opts *BindOptions) (err error) {
-	// FIXME
-	fmt.Println("TODO")
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return
+	}
+
+	body := models.BindImageToProjectCommand{
+		ProjectID: opts.ProjectID,
+		Images:    opts.ImageIDs,
+	}
+
+	params := images.NewImagesBindImagesToProjectParams().WithV(api.Version)
+	params = params.WithBody(&body)
+
+	_, err = apiClient.Client.Images.ImagesBindImagesToProject(params, apiClient)
+	if err == nil {
+		out.PrintStandardSuccess()
+	}
+
 	return
 }
