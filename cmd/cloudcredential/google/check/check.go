19b3f7d969b5fe6d020452f8be5835e0fd0421fa
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-03-07 12:49:42 +0100

    feat(gcp): implement check command

--- cmd/cloudcredential/google/check/check.go
+++ cmd/cloudcredential/google/check/check.go
@@ -1,13 +1,17 @@
 package check
 
 import (
-	"fmt"
+	"os"
 
+	"github.com/itera-io/taikun-cli/api"
+	"github.com/itera-io/taikun-cli/cmd/cmderr"
+	"github.com/itera-io/taikun-cli/utils/out"
+	"github.com/itera-io/taikungoclient/client/checker"
 	"github.com/spf13/cobra"
 )
 
 type CheckOptions struct {
-	// FIXME
+	ConfigFilePath string
 }
 
 func NewCmdCheck() *cobra.Command {
@@ -18,18 +22,34 @@ func NewCmdCheck() *cobra.Command {
 		Short: "Check the validity of a Google Cloud Platform credential",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			// FIXME
+			opts.ConfigFilePath = args[0]
 			return checkRun(&opts)
 		},
 	}
-
-	// FIXME
 
 	return &cmd
 }
 
 func checkRun(opts *CheckOptions) (err error) {
-	// FIXME
-	fmt.Println("TODO")
+	apiClient, err := api.NewClient()
+	if err != nil {
+		return err
+	}
+
+	configFile, err := os.Open(opts.ConfigFilePath)
+	if err != nil {
+		return err
+	}
+
+	params := checker.NewCheckerGoogleParams().WithV(api.Version)
+	params = params.WithConfig(configFile)
+
+	_, err = apiClient.Client.Checker.CheckerGoogle(params, apiClient)
+	if err == nil {
+		out.PrintCheckSuccess("Google Cloud Platform credential")
+	} else if _, isValidationProblem := err.(*checker.CheckerAwsBadRequest); isValidationProblem {
+		return cmderr.ErrCheckFailure("Google Cloud Platform credential")
+	}
+
 	return
 }
