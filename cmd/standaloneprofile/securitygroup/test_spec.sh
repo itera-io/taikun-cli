3429c91b44286e720b567d9077b0649ef4d77f26
raphael <raphael.nivault@itera.io>
2022-12-13 11:51:39 +0100

    Fix(tests): Fix remote ip in standaloneprofile/securitygroup test

--- cmd/standaloneprofile/securitygroup/test_spec.sh
+++ cmd/standaloneprofile/securitygroup/test_spec.sh
@@ -2,9 +2,9 @@ Context 'standaloneprofile/securitygroup'
 
   setup() {
     id=$(taikun standalone-profile add $(_rnd_name) --public-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHshx25CJGDd0HfOQqNt65n/970dsPt0y12lfKKO9fAs dummy" -I)
-    udp_id=$(taikun standalone-profile security-group add $id --name "doom" --protocol udp --min-port 666 --max-port 666 --remote-ip-prefix '192.0.2' -I)
-    tcp_id=$(taikun standalone-profile security-group add $id --name "rsync" --protocol tcp --min-port 873 --max-port 873 --remote-ip-prefix '172.16.0' -I)
-    icmp_id=$(taikun standalone-profile security-group add $id --name "icmp" --protocol icmp --remote-ip-prefix '192.168' -I)
+    udp_id=$(taikun standalone-profile security-group add $id --name "doom" --protocol udp --min-port 666 --max-port 666 --remote-ip-prefix '0.0.0.0/0' -I)
+    tcp_id=$(taikun standalone-profile security-group add $id --name "rsync" --protocol tcp --min-port 873 --max-port 873 --remote-ip-prefix '0.0.0.0/0' -I)
+    icmp_id=$(taikun standalone-profile security-group add $id --name "icmp" --protocol icmp --remote-ip-prefix '0.0.0.0/0' -I)
   }
   BeforeAll 'setup'
 
@@ -26,7 +26,7 @@ Context 'standaloneprofile/securitygroup'
   End
 
   Example 'calling add without standalone profile ID should cause error'
-    When call taikun standalone-profile security-group add --name "doom2" --protocol udp --min-port 666 --max-port 666 --remote-ip-prefix '192.0.2'
+    When call taikun standalone-profile security-group add --name "doom2" --protocol udp --min-port 666 --max-port 666 --remote-ip-prefix '0.0.0.0/0'
     The status should equal 1
     The stderr should equal 'Error: accepts 1 arg(s), received 0'
   End
@@ -38,25 +38,25 @@ Context 'standaloneprofile/securitygroup'
   End
 
   Example 'calling add without max port should cause error'
-    When call taikun standalone-profile security-group add $id --name "doom2" --protocol udp --min-port 666 --remote-ip-prefix '192.0.2'
+    When call taikun standalone-profile security-group add $id --name "doom2" --protocol udp --min-port 666 --remote-ip-prefix '0.0.0.0/0'
     The status should equal 1
     The stderr should include 'max-port'
   End
 
   Example 'calling add without min port should cause error'
-    When call taikun standalone-profile security-group add $id --name "doom2" --protocol udp --max-port 666 --remote-ip-prefix '192.0.2'
+    When call taikun standalone-profile security-group add $id --name "doom2" --protocol udp --max-port 666 --remote-ip-prefix '0.0.0.0/0'
     The status should equal 1
     The stderr should include 'min-port'
   End
 
   Example 'adding ICMP with port range should cause error'
-    When call taikun standalone-profile security-group add $id --name "doom2" --protocol icmp --min-port 666 --max-port 666 --remote-ip-prefix '192.0.2'
+    When call taikun standalone-profile security-group add $id --name "doom2" --protocol icmp --min-port 666 --max-port 666 --remote-ip-prefix '0.0.0.0/0'
     The status should equal 1
     The stderr should include 'port range'
   End
 
   Example 'duplicate name should cause error'
-    When call taikun standalone-profile security-group add $id --name "doom" --protocol udp --min-port 666 --max-port 666 --remote-ip-prefix '192.0.2'
+    When call taikun standalone-profile security-group add $id --name "doom" --protocol udp --min-port 666 --max-port 666 --remote-ip-prefix '0.0.0.0/0'
     The status should equal 1
     The stderr should include 'Please specify another name'
   End
