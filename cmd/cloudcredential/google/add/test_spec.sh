b57f722d723ae5b1f18d9d3c83a7ef25070fddfc
raphael <raphael.nivault@itera.io>
2022-12-13 09:07:17 +0100

    Fix(tests): put a number in 'azcount' parameter google CC

--- cmd/cloudcredential/google/add/test_spec.sh
+++ cmd/cloudcredential/google/add/test_spec.sh
@@ -1,25 +1,25 @@
 Context 'cloudcredential/google/add'
 
   Example 'Cannot set both import-project and billing-account-id'
-    When call taikun cloud-credential google add g --region g --az-count g --import-project --billing-account-id "foo" --config-file g
+    When call taikun cloud-credential google add g --region g --az-count 1 --import-project --billing-account-id "foo" --config-file g
     The stderr should include "The flags --import-project and --billing-account-id are mutually exclusive"
     The status should equal 1
   End
 
   Example 'Cannot set both import-project and folder-id'
-    When call taikun cloud-credential google add g --region g --az-count g --import-project --folder-id "foo" --config-file g
+    When call taikun cloud-credential google add g --region g --az-count 1 --import-project --folder-id "foo" --config-file g
     The stderr should include "The flags --import-project and --folder-id are mutually exclusive"
     The status should equal 1
   End
 
   Example 'Must set billing-account-id if not importing a project'
-    When call taikun cloud-credential google add g --region g --az-count g --folder-id "foo" --config-file g
+    When call taikun cloud-credential google add g --region g --az-count 1 --folder-id "foo" --config-file g
     The stderr should include "Must set --billing-acount-id if not importing a project"
     The status should equal 1
   End
 
   Example 'Must set folder-id if not importing a project'
-    When call taikun cloud-credential google add g --region g --az-count g --billing-account-id "foo" --config-file g
+    When call taikun cloud-credential google add g --region g --az-count 1 --billing-account-id "foo" --config-file g
     The stderr should include "Must set --folder-id if not importing a project"
     The status should equal 1
   End
