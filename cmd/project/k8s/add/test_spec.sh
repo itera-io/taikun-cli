945bd512f447fe9525c444457f329e3c3822d48f
raphael <raphael.nivault@itera.io>
2022-12-15 14:26:04 +0100

    Add(servers): Add tests for the availability-zone feature

--- cmd/project/k8s/add/test_spec.sh
+++ cmd/project/k8s/add/test_spec.sh
@@ -40,3 +40,35 @@ Context 'project/k8s/add'
     End
   End
 End
+
+Context 'project/k8s/add'
+
+  setup() {
+    oid=$(taikun organization add g -f ggg)
+    ccid=$(taikun cloud-credential aws add $(_rnd_name) -a $AWS_ACCESS_KEY_ID -s $AWS_SECRET_ACCESS_KEY -r $AWS_DEFAULT_REGION -z 1 -o $oid -I)
+    flavor=$(taikun cloud-credential flavors $ccid --no-decorate --min-cpu 4 --max-cpu 4 --min-ram 8 --max-ram 8 -C name --limit 1)
+    pid=$(taikun project add $(_rnd_name) --cloud-credential-id $ccid --flavors $flavor -I)
+  }
+  BeforeAll 'setup'
+
+  cleanup() {
+    if ! taikun project delete $pid -q 2>/dev/null; then
+      taikun project delete --force $pid -q 2>/dev/null || true
+    fi
+    taikun cloud-credential delete $ccid -q 2>/dev/null || true
+    taikun organization delete $oid
+  }
+  AfterAll 'cleanup'
+
+  Context
+    remove_master() {
+      taikun project k8s delete $pid -a -q 2>/dev/null || true
+    }
+    AfterAll 'remove_master'
+
+    Example 'add one server with availability zone'
+      When call msid=$(taikun project k8s add $pid -n master --flavor $flavor -r kubemaster -a a)
+      The status should equal 0
+    End
+  End
+End
