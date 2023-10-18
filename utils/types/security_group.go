b0307cedc2865d9bf71cc3eff233a8e7572742ae
Radek Smid <radek.smid@taikun.cloud>
2023-10-18 17:54:04 +0200

    fix: Standalone profiles are using a rewritten type.

--- utils/types/security_group.go
+++ utils/types/security_group.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"github.com/itera-io/taikun-cli/utils/gmap"
+	taikuncore "github.com/itera-io/taikungoclient/client"
+)
+
 //import (
 //	"github.com/itera-io/taikun-cli/utils/gmap"
 //	"github.com/itera-io/taikungoclient/models"
@@ -17,3 +22,16 @@ package types
 //	model, _ := SecurityGroupProtocols.Get(protocol).(models.SecurityGroupProtocol)
 //	return model
 //}
+
+var SecurityGroupProtocols = gmap.New(
+	map[string]interface{}{
+		"icmp": taikuncore.SECURITYGROUPPROTOCOL_ICMP,
+		"tcp":  taikuncore.SECURITYGROUPPROTOCOL_TCP,
+		"udp":  taikuncore.SECURITYGROUPPROTOCOL_UDP,
+	},
+)
+
+func GetSecurityGroupProtocol(protocol string) taikuncore.SecurityGroupProtocol {
+	model, _ := SecurityGroupProtocols.Get(protocol).(taikuncore.SecurityGroupProtocol)
+	return model
+}
