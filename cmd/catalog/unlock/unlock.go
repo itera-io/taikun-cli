955639cb1af7f5634df2bad6aa879da73c12465b
Radek Smid <radek.smid@taikun.cloud>
2025-09-22 16:58:18 +0200

    feat: catalog lock and unlock

---
+++ cmd/catalog/unlock/unlock.go
@@ -0,0 +1,50 @@
+package unlock
+
+import (
+	"context"
+
+	"github.com/itera-io/taikun-cli/cmd/cmderr"
+	"github.com/itera-io/taikun-cli/utils/out"
+	"github.com/itera-io/taikun-cli/utils/types"
+	tk "github.com/itera-io/taikungoclient"
+	taikuncore "github.com/itera-io/taikungoclient/client"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdUnlock() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "unlock <catalog-id>",
+		Short: "Unlock a catalog",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			id, err := types.Atoi32(args[0])
+			if err != nil {
+				return cmderr.ErrIDArgumentNotANumber
+			}
+			return unlockRun(id)
+		},
+	}
+
+	return cmd
+}
+
+func unlockRun(catalogID int32) (err error) {
+	// Create and authenticated client to the Taikun API
+	myApiClient := tk.NewClient()
+
+	// Prepare the arguments for the query
+	body := taikuncore.CatalogLockManagementCommand{
+		Id:   &catalogID,
+		Mode: *taikuncore.NewNullableString(&types.UnlockedMode),
+	}
+
+	// Execute a query into the API + graceful exit
+	response, err := myApiClient.Client.CatalogAPI.CatalogLock(context.TODO()).CatalogLockManagementCommand(body).Execute()
+	if err != nil {
+		return tk.CreateError(response, err)
+	}
+
+	out.PrintStandardSuccess()
+	return
+
+}
